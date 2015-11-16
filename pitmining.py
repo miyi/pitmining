9501595826cc1af56fde1afd4945a6b08604c18b
Jeff Taylor <jefftaylor42@gmail.com>
2015-11-15 22:11:08 -0800

    Make things a little more flexible in prep for real data.

--- pitmining.py
+++ pitmining.py
@@ -5,43 +5,33 @@ import math
 import numpy as np
 from matplotlib import pyplot
 
-def main():
-    try:
-        width = int(sys.argv[1])
-    except IndexError:
-        print("usage: {} <dim>".format(sys.argv[0]))
-        exit(1)
-
-    # Since the maximum angle is 45°, we only need our grid to be half as deep as
-    # it is wide
-    depth = int(math.ceil(width / 2))
-
+def do_pitmine(price):
+    zlim,ylim,xlim = price.shape
     prob = pulp.LpProblem("pitmine", pulp.LpMaximize)
 
     # Create a dict of variables "d_zyx"
     ds = {}
-    for z in range(depth):
-        for y in range(width):
-            for x in range(width):
-                if (z <= y < width - z) and (z <= x < width - z):
+    for z in range(zlim):
+        for y in range(ylim):
+            for x in range(xlim):
+                if (z <= y < ylim - z) and (z <= x < xlim - z):
                     # Give our variables names like d003002001 for 3-deep,
                     # 2-across.  These names aren't actually used anywhere,
                     # but PuLP makes them mandatory and they have to be
                     # unique.
                     name = "d{:03}{:03}{:03}".format(z, y, x)
-                    ds.update({(z,y,x):
-                            pulp.LpVariable(name, cat=pulp.LpBinary)})
+                    ds[z,y,x] = pulp.LpVariable(name, cat=pulp.LpBinary)
                 else:
                     # These cells are guaranteed to be zero, no need to
                     # include them in the model.
-                    ds.update({(z,y,x): 0})
+                    ds[z,y,x] = 0
 
                 if z == 0:
                     continue
 
                 def addconstraint(dy, dx):
                     nonlocal prob
-                    if (0 <= y + dy < width and 0 <= x + dx < width):
+                    if (0 <= y + dy < ylim and 0 <= x + dx < xlim):
                         prob += ds[z,y,x] <= ds[z-1,y+dy,x+dx]
 
                 # Add nine constraints, one per block above this one
@@ -55,16 +45,14 @@ def main():
                 addconstraint(1, 0)
                 addconstraint(1, 1)
 
-    # Maximizing profit from randomly valued dirt.
-    from random import seed, normalvariate
-    seed(8) # Tried seeds until I got a "nice" mine.
-    prob += sum(d * normalvariate(0.02, 0.25) for d in ds.values())
+    # Maximizing profit
+    prob += sum(ds[z,y,x] * price[z,y,x] for z,y,x in ds)
 
     outcome = prob.solve()
     print(pulp.LpStatus[outcome])
 
     # Plot an image
-    image = np.zeros((depth,width,width))
+    image = np.zeros((zlim,ylim,xlim))
     for z, y, x in ds:
         image[z,y,x] = pulp.value(ds[z,y,x])
 
@@ -75,13 +63,33 @@ def main():
     from mpl_toolkits.mplot3d import Axes3D
     fig = pyplot.figure()
     ax = fig.add_subplot(111, projection='3d')
-    x = np.arange(0, width)
-    x, y = np.meshgrid(x, x)
+    x = np.arange(0, xlim)
+    y = np.arange(0, ylim)
+    x, y = np.meshgrid(y, x)
     ax.plot_surface(x, y, -np.sum(image, 0), cmap='terrain', rstride=1,
             cstride=1, linewidth=0)
-    ax.set_zlim(-width/1.4, 0)
+    ax.set_zlim(-xlim/1.4, 0) # hack to make it look square
     pyplot.show()
     pyplot.imsave('output.png', np.sum(image, 0), cmap='terrain')
+
+lamb = 1.03
+
+def main():
+    try:
+        xlim = int(sys.argv[1])
+    except IndexError:
+        print("usage: {} <dim>".format(sys.argv[0]))
+        exit(1)
+
+    ylim = xlim
+
+    # Since the maximum angle is 45°, we only need our grid to be half as deep
+    # as it is wide
+    zlim = int(math.ceil(xlim / 2))
+
+    import scipy.stats
+    price = scipy.stats.expon.rvs(loc=-1, scale=lamb, size=(zlim, ylim, xlim))
+    do_pitmine(price)
 
 if __name__ == "__main__":
     main()
