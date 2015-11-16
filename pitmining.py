3e736e0826dc6a79a7a5b9f008be97f3268a57fd
Jeff Taylor <jefftaylor42@gmail.com>
2015-11-15 21:36:25 -0800

    Make it 3D

--- pitmining.py
+++ pitmining.py
@@ -2,6 +2,8 @@
 import pulp
 import sys
 import math
+import numpy as np
+from matplotlib import pyplot
 
 def main():
     try:
@@ -14,54 +16,72 @@ def main():
     # it is wide
     depth = int(math.ceil(width / 2))
 
-    # Create a 2d array of variables "d_zy"
-    ds = []
-    for z in range(depth):
-        arr = []
-        for y in range(width):
-            if z <= y and y <= width - z:
-                # Give our variables names like d003002 for 3-deep, 2-across.
-                # These names aren't actually used anywhere, but PuLP makes them
-                # mandatory.
-                name = "d{:03}{:03}".format(z, y)
-                arr.append(pulp.LpVariable(name, cat=pulp.LpBinary))
-            else:
-                # These cells are guaranteed to be zero, no need to include them
-                # in the model.
-                arr.append(0)
-        ds.append(arr)
-
-
     prob = pulp.LpProblem("pitmine", pulp.LpMaximize)
 
-    # Slope angle constraints
-    for y in range(width):
-        for z in range(1,depth):
-            # Add three constraints, one per block above this one
-            prob += ds[z][y] <= ds[z-1][y]
+    # Create a dict of variables "d_zyx"
+    ds = {}
+    for z in range(depth):
+        for y in range(width):
+            for x in range(width):
+                if (z <= y < width - z) and (z <= x < width - z):
+                    # Give our variables names like d003002001 for 3-deep,
+                    # 2-across.  These names aren't actually used anywhere,
+                    # but PuLP makes them mandatory and they have to be
+                    # unique.
+                    name = "d{:03}{:03}{:03}".format(z, y, x)
+                    ds.update({(z,y,x):
+                            pulp.LpVariable(name, cat=pulp.LpBinary)})
+                else:
+                    # These cells are guaranteed to be zero, no need to
+                    # include them in the model.
+                    ds.update({(z,y,x): 0})
 
-            if y > 0:
-                prob += ds[z][y] <= ds[z-1][y-1]
+                if z == 0:
+                    continue
 
-            if y < width - 1:
-                prob += ds[z][y] <= ds[z-1][y+1]
+                def addconstraint(dy, dx):
+                    nonlocal prob
+                    if (0 <= y + dy < width and 0 <= x + dx < width):
+                        prob += ds[z,y,x] <= ds[z-1,y+dy,x+dx]
+
+                # Add nine constraints, one per block above this one
+                addconstraint(-1, -1)
+                addconstraint(-1, 0)
+                addconstraint(-1, 1)
+                addconstraint(0, -1)
+                addconstraint(0, 0)
+                addconstraint(0, 1)
+                addconstraint(1, -1)
+                addconstraint(1, 0)
+                addconstraint(1, 1)
 
     # Maximizing profit from randomly valued dirt.
-    from random import uniform, seed
+    from random import seed, normalvariate
     seed(8) # Tried seeds until I got a "nice" mine.
-    values = [[uniform(-1, 1) for i in range(width)] for j in range(depth)]
-
-    prob += sum(sum(d*v for d, v in zip(d1,v1)) for d1,v1 in
-            zip(ds, values)), "objective"
+    prob += sum(d * normalvariate(0.02, 0.25) for d in ds.values())
 
     outcome = prob.solve()
     print(pulp.LpStatus[outcome])
 
     # Plot an image
-    from matplotlib import pyplot
-    import numpy
-    image = numpy.array([[int(pulp.value(d)) for d in d1] for d1 in ds])
-    pyplot.imsave('output.png', image, cmap='copper')
+    image = np.zeros((depth,width,width))
+    for z, y, x in ds:
+        image[z,y,x] = pulp.value(ds[z,y,x])
+
+    # Print out how deep we went (a birds-eye view of the mine depth)
+    print(np.sum(image, 0))
+
+    # Do a 3D plot of the mine
+    from mpl_toolkits.mplot3d import Axes3D
+    fig = pyplot.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    x = np.arange(0, width)
+    x, y = np.meshgrid(x, x)
+    ax.plot_surface(x, y, -np.sum(image, 0), cmap='terrain', rstride=1,
+            cstride=1, linewidth=0)
+    ax.set_zlim(-width/1.4, 0)
+    pyplot.show()
+    pyplot.imsave('output.png', np.sum(image, 0), cmap='terrain')
 
 if __name__ == "__main__":
     main()
